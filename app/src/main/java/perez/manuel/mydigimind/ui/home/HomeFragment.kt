e7e62f50f998984c67c4ba3a54d3cb78f7e1483e
Bell-789 <159948621+Bell-789@users.noreply.github.com>
2025-02-18 22:56:44 -0700

    Initial commit

---
+++ app/src/main/java/perez/manuel/mydigimind/ui/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package perez.manuel.mydigimind.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import perez.manuel.mydigimind.R
+import perez.manuel.mydigimind.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        homeViewModel.text.observe(viewLifecycleOwner,{
+
+        })
+        return root
+    }
+}
